ce4e90c2880ec16a45edbfe825d7fc504750cabf
lggerman <lopez.ggerman@gmail.com>
2021-05-12 04:48:34 -0300

    bugfix

--- index.js
+++ index.js
@@ -16,42 +16,7 @@ $PROJECTS.forEach((item, i) => {
   });
 });
 
-/* verifyName("nan1o");
-function verifyName(name) {
-  if (/^[a-zA-Z]+$/.test(name)) {
-    console.log("anduvo papá");
-    name.classList.delete("form-error");
-    $ERROR.innerText = "";
-  } else {
-    name.classList.add("form-error");
-    $ERROR.innerText =
-      "El nombre u apellido no debe contener numeros ni caracteres especiales";
-  }
-}
-function verifyEmptyString(string) {
-  const stringNoSpaces = string.value.replace(/\s/g, "");
-  if (!stringNoSpaces == "") {
-    console.log("no ta vacio");
-    string.classList.remove("form-error");
-    $ERROR.innerText = "";
-  } else {
-    string.classList.add("form-error");
-    $ERROR.innerText = "El nombre u email no debe estar vacio";
-  }
-}
-
-function verifyForm() {
-  const name = document.querySelector("#name");
-  const last_name = document.querySelector("#last-name");
-  const email = document.querySelector("#email");
-
-  verifyEmptyString(name);
-  verifyEmptyString(email);
-  verifyName(name);
-  console.log(name.value, last_name.value, email.value);
-} */
 const $FORM = document.querySelector("#form-contact");
-console.log($FORM);
 async function handleSubmit(event) {
   event.preventDefault();
   var status = document.querySelector("#status");
